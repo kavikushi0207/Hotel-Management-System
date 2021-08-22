e100837f6b791923ff037f31aa295bea9d3464f8
Kavini Madhusanka Kushani <62046302+kavikushi0228@users.noreply.github.com>
2021-08-22 21:53:11 +0530

    Add project files.

---
+++ GUI Group Project/DashBoard.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_Group_Project
+{
+    /// <summary>
+    /// Interaction logic for DashBoard.xaml
+    /// </summary>
+    public partial class DashBoard : Window
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow login = new MainWindow();
+            login.Show();
+            login.txtpassword.Clear();
+            login.txtuser.Clear();
+            this.Close();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewGuest NG = new NewGuest();
+            NG.Show();
+            this.Close();
+        }
+    }
+}
---
+++ GUI Group Project/NewGuest.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_Group_Project
+{
+    /// <summary>
+    /// Interaction logic for NewGuest.xaml
+    /// </summary>
+    public partial class NewGuest : Window
+    {
+        public NewGuest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnback_Click(object sender, RoutedEventArgs e)
+        {
+            DashBoard DB = new DashBoard();
+            DB.Show();
+            this.Close();
+        }
+
+        private void btnLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow login = new MainWindow();
+            login.Show();
+            login.txtpassword.Clear();
+            login.txtuser.Clear();
+            this.Close();
+        }
+    }
+}
